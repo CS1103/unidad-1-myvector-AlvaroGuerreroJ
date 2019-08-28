a1fb960eb596a9e5176fba92c6055d241d23267a
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2019-08-27 21:54:14 -0500

    Implement quite some things. Should be in working state.

--- src/MyVector.hpp
+++ src/MyVector.hpp
@@ -1,7 +1,10 @@
 #ifndef GUARD_MYVECTOR_HPP
 #define GUARD_MYVECTOR_HPP
 
+#include <algorithm>
 #include <cstddef>
+#include <initializer_list>
+#include <stdexcept>
 
 template<class T>
 class MyVector
@@ -17,16 +20,25 @@ public:
 
 public:
     MyVector();
+    MyVector(const MyVector<T>& other);
+    MyVector(MyVector<T>&& other);
+    MyVector(std::initializer_list<T> il);
+    template<class It>
+    MyVector(It beg, It end);
+    MyVector(size_type count, const T& value = T());
     ~MyVector();
 
     void push_back(const T& value);
     void pop_back();
 
     value_type& operator[](size_type pos);
+    const value_type& operator[](size_type pos) const;
     value_type& at(size_type pos);
+    const value_type& at(size_type pos) const;
 
     size_type size() const;
     MyVector<T> operator+(const MyVector<T>& other);
+    MyVector<T>& operator=(const MyVector<T>& other);
 };
 
 template<class T>
@@ -35,6 +47,59 @@ MyVector<T>::MyVector()
     m_vec_arr = nullptr;
     m_size = 0;
     m_reserved = 0;
+}
+
+template<class T>
+MyVector<T>::MyVector(const MyVector<T>& other)
+{
+    m_size = other.size();
+    m_reserved = other.size();
+    m_vec_arr = new T[other.size()];
+    std::copy(other.m_vec_arr, other.m_vec_arr + other.size(), m_vec_arr);
+}
+
+template<class T>
+MyVector<T>::MyVector(MyVector<T>&& other)
+{
+    m_size = other.size();
+    m_reserved = other.size();
+    m_vec_arr = other.m_vec_arr;
+    other.m_size = 0;
+    other.m_reserved = 0;
+    other.m_vec_arr = nullptr;
+}
+
+template<class T>
+MyVector<T>::MyVector(MyVector<T>::size_type count, const T& value)
+{
+    m_vec_arr = new T[count];
+    m_reserved = count;
+    m_size = count;
+
+    for (size_type i = 0; i < count; i++)
+    {
+        m_vec_arr[i] = value;
+    }
+}
+
+template<class T>
+MyVector<T>::MyVector(std::initializer_list<T> il)
+{
+    m_reserved = il.size();
+    m_size = il.size();
+    m_vec_arr = new T[il.size()];
+    std::copy(il.begin(), il.end(), m_vec_arr);
+}
+
+template<class T>
+template<class It>
+MyVector<T>::MyVector(It beg, It end)
+{
+    size_type sz = std::distance(beg, end);
+    m_reserved = sz;
+    m_size = sz;
+    m_vec_arr = new T[sz];
+    std::copy(beg, end, m_vec_arr);
 }
 
 template<class T>
@@ -49,11 +114,22 @@ MyVector<T>::~MyVector()
 template<class T>
 void MyVector<T>::push_back(const T& value)
 {
-    // TODO
+    if (m_size == m_reserved)
+    {
+        // TODO: This should probably be another method
+        m_reserved = std::max(m_reserved, size_type(1));
+        m_reserved <<= 1;
+        T* temp = m_vec_arr;
+        m_vec_arr = new T[m_reserved];
+        std::copy(temp, temp + m_size, m_vec_arr);
+        delete[] temp;
+    }
+    m_vec_arr[m_size] = value;
+    m_size++;
 }
 
 template<class T>
-MyVector<T>::size_type MyVector<T>::size() const
+typename MyVector<T>::size_type MyVector<T>::size() const
 {
     return m_size;
 }
@@ -61,25 +137,77 @@ MyVector<T>::size_type MyVector<T>::size() const
 template<class T>
 void MyVector<T>::pop_back()
 {
-    // TODO
+    if (m_size > 0)
+    {
+        m_size -= 1;
+    }
 }
 
 template<class T>
-MyVector<T>::value_type& MyVector<T>::operator[](size_type pos)
+typename MyVector<T>::value_type& MyVector<T>::operator[](size_type pos)
 {
-    // TODO
+    // operator[] doesn't perform bound checking
+    return m_vec_arr[pos];
 }
 
 template<class T>
-MyVector<T>::value_type& MyVector<T>::at(size_type pos)
+const typename MyVector<T>::value_type& MyVector<T>::operator[](size_type pos) const
 {
-    // TODO
+    // operator[] doesn't perform bound checking
+    return m_vec_arr[pos];
+}
+
+template<class T>
+typename MyVector<T>::value_type& MyVector<T>::at(size_type pos)
+{
+    if (pos >= m_size)
+    {
+        // TODO: Add explanatory string
+        throw std::out_of_range();
+    }
+    return m_vec_arr[pos];
+}
+
+template<class T>
+const typename MyVector<T>::value_type& MyVector<T>::at(size_type pos) const
+{
+    if (pos >= m_size)
+    {
+        // TODO: Add explanatory string
+        throw std::out_of_range();
+    }
+    return m_vec_arr[pos];
 }
 
 template<class T>
 MyVector<T> MyVector<T>::operator+(const MyVector<T>& other)
 {
-    // TODO
+    size_type new_size = this->size() + other.size();
+    MyVector ret(new_size);
+
+    // TODO: I think this could be prettier
+    for (size_type i = 0; i < this->size(); i++)
+    {
+        ret[i] = (*this)[i];
+    }
+    for (size_type i = this->size(); i < this->size() + other.size(); i++)
+    {
+        ret[i] = other[i - this->size()];
+    }
+
+    return ret;
+}
+
+template<class T>
+MyVector<T>& MyVector<T>::operator=(const MyVector<T>& other)
+{
+    m_reserved = other.size();
+    m_size = other.size();
+    m_vec_arr = new T[other.size()];
+
+    std::copy(other.m_vec_arr, other.m_vec_arr + other.size(), m_vec_arr);
+
+    return *this;
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,62 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
 #include "MyVector.hpp"
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+template<class T>
+void print_vector(const MyVector<T>& vec);
 
 int main()
 {
-    MyVector<int> nums;
+    // FIXME: This won't work, can't think of a way to solve it.
+    // MyVector<int> nums0(10, 121);
+    // cout << "nums0:" << endl;
+    // print_vector(nums0);
+
+    MyVector<int> nums1;
+    nums1.push_back(10);
+    nums1.push_back(20);
+    nums1.push_back(30);
+    cout << "nums1:" << endl;
+    print_vector(nums1);
+
+    MyVector<int> nums2 = {1, 4, 9, 16, 25, 36};
+    cout << "nums2:" << endl;
+    print_vector(nums2);
+
+    MyVector<int> nums3 = nums1 + nums2;
+    cout << "nums3:" << endl;
+    print_vector(nums3);
+
+    MyVector<int> nums4(nums1);
+    cout << "nums4:" << endl;
+    print_vector(nums4);
+
+    MyVector<int> nums5(nums1 + nums2);
+    cout << "nums5:" << endl;
+    print_vector(nums5);
+
+    nums5 = nums1;
+    cout << "nums5:" << endl;
+    print_vector(nums5);
+
+    vector<string> v = {"hello", "world"};
+    MyVector<string> words0(v.begin(), v.end());
+    cout << "words0:" << endl;
+    print_vector(words0);
+}
+
+template<class T>
+void print_vector(const MyVector<T>& vec)
+{
+    for (typename MyVector<T>::size_type i = 0; i < vec.size(); i++)
+    {
+        cout << vec[i] << endl;
+    }
 }
